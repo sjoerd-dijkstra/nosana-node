cf0b22cd439c35495fff384527185eabd3e043c4
Sjoerd Dijkstra <sjoerd@yogy.io>
2023-02-15 18:49:19 +0100

    update action

--- .github/scripts/prepare-release.sh
+++ .github/scripts/prepare-release.sh
@@ -50,8 +50,6 @@ EOF
 echo "## SHA 256" > "../${RELEASE_BODY_FILE}"
 for file in *.sha256sum
 do
-  all_files_sha=
-
   echo "- \`$( cat "${file}" | cut -f 1 -d ' ' )\` ${file}" >> "../${RELEASE_BODY_FILE}"
 done
 
@@ -62,5 +60,7 @@ EOF
 echo "${CHANGE_LOG}" >> "../${RELEASE_BODY_FILE}"
 
 # github output
-echo new_version="${version}" >> "${GITHUB_OUTPUT}"
-echo all_files_sha="${all_files_sha}" > "${GITHUB_OUTPUT}"
+for output in "new_version=${version}" "all_files_sha=${all_files_sha}"
+do
+  echo "${output}" >> "$GITHUB_OUTPUT"
+done
