41a961eec2c70a18414bd386f21d24c65ac6fe4a
Sjoerd Dijkstra <sjoerd@yogy.io>
2023-02-15 16:39:19 +0100

    update action

--- .github/scripts/prepare-release.sh
+++ .github/scripts/prepare-release.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
 
+# exit upon failure
+set -e
+
+# setup
+[[ -n "${DEBUG_SCRIPT}" ]] && set -xv
+
+# shell swag
+RED="\033[1;91m"
+CYAN="\033[1;36m"
+GREEN="\033[1;32m"
+WHITE="\033[1;38;5;231m"
+RESET="\n\033[0m"
+
+# logging
+log_std() { echo -e "${CYAN}==> ${WHITE}${1}${RESET}"; }
+log_err() { echo -e "${RED}==> ${WHITE}${1}${RESET}"; }
+
 # vars
 version=${tag_ref/refs\/tags\//}
+log_std "Preparing release ${GREEN}${version}"
 
 # create release directory
 mkdir release
